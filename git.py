3f9de42a8a566eccbfab26627ecbb1a6a700ae86
Roc <3149334565@qq.com>
2024-10-03 20:37:42 +0800

    "git"

--- git.py
+++ git.py
@@ -2,20 +2,28 @@ import subprocess
 import os
 
 def git_process(folder_path):
+    # 定义压缩文件的扩展名
+    compress_extensions = {'.zip', '.rar', '.tar.gz', '.gz', '.7z'}
+
     # 遍历文件夹及其子文件夹中的所有文件
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             print(file_path)
             try:
-                # 获取不带扩展名的文件名
-                filename_without_extension = os.path.splitext(file)[0]
+                # 检查文件是否是压缩文件
+                if os.path.splitext(file)[1].lower() in compress_extensions:
+                    # 如果是压缩文件，直接执行命令
+                    commit_message = f'"Update compressed file: {file}"'
+                else:
+                    # 获取不带扩展名的文件名
+                    filename_without_extension = os.path.splitext(file)[0]
+                    commit_message = f'"{filename_without_extension}"'
 
                 # 执行 git add
                 subprocess.run(['git', 'add', file_path], check=True)
 
                 # 执行 git commit
-                commit_message = f'"{filename_without_extension}"'
                 subprocess.run(['git', 'commit', '-m', commit_message], check=True, shell=True)
 
                 # 执行 git push
