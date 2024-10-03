9bfa619203039e290b839649aa20c4c6a566ef00
Roc <3149334565@qq.com>
2024-10-03 20:33:37 +0800

    "git"

--- git.py
+++ git.py
@@ -8,15 +8,18 @@ def git_process(folder_path):
             file_path = os.path.join(root, file)
             print(file)
             try:
+                # 获取不带扩展名的文件名
+                filename_without_extension = os.path.splitext(file)[0]
+
                 # 执行 git add
                 subprocess.run(['git', 'add', file_path], check=True)
 
                 # 执行 git commit
-                commit_message = f'"{file}"'
-                subprocess.run(['git', 'commit', '-m', commit_message], check=True)
+                commit_message = f'"{filename_without_extension}"'
+                subprocess.run(['git', 'commit', '-m', commit_message], check=True, shell=True)
 
                 # 执行 git push
-                subprocess.run(['git', 'push', 'origin', 'main'], check=True)
+                subprocess.run(['git', 'push', 'origin', 'main'], check=True, shell=True)
 
                 print(f'Processed {file_path}')
             except subprocess.CalledProcessError as e:
@@ -25,7 +28,7 @@ def git_process(folder_path):
     print('All files have been processed.')
 
 # 指定要遍历的文件夹路径
-folder_path = './'
+folder_path = r'C:\cdpan\2023面试大全'
 
 # 调用函数
 git_process(folder_path)
